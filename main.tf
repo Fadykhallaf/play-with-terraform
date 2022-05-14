d6f180d5f7d9710d8d5af6651bc352fb4dd78fc7
Fady Khallaf <fady.khallaf@gmail.com>
2022-05-14 14:48:46 +0200

    using maps and lists

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 # local terraform provider
 resource "local_file" "test" {
     filename = var.filename
-    content = var.content
+    content = var.content["stat2"]
 }
 
 # random terraform provider
 
 resource "random_pet" "petto" {
-    prefix = var.prefix
+    prefix = var.prefix["1"]
     separator = var.separator
     length = var.length
 }
